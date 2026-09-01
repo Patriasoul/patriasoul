8525063ec35e33276d008c4ef38d2b14b12644ca
Patriasoul <patriasoul@protonmail.com>
2026-09-01 13:36:00 +0200

    Faza 2: kanonski registar izvora

---
+++ sources-phase2.js
@@ -0,0 +1,13 @@
+// PatriaSoul – kanonski registar uredničkih izvora (Faza 2)
+// Izvor se veže uz sadržaj; ne kopirati zaštićene tekstove iz izvora.
+window.PATRIA_SOURCES={
+  "mpudt-jls":{title:"Popis županija, gradova i općina",publisher:"Ministarstvo pravosuđa, uprave i digitalne transformacije RH",url:"https://mpudt.gov.hr/o-ministarstvu/ustrojstvo/uprava-za-politicki-sustav-i-opcu-upravu/lokalna-i-podrucna-regionalna-samouprava/popis-zupanija-gradova-i-opcina/22319",type:"official",use:["teritorijalni ustroj","popis gradova","županije"]},
+  "data-gov-jls":{title:"Popis županija, gradova i općina",publisher:"Portal otvorenih podataka Republike Hrvatske",url:"https://data.gov.hr/ckan/hr/dataset/popis-zupanija-gradova-i-opcina",type:"official-open-data",use:["teritorijalni ustroj","otvoreni podaci"]},
+  "dzs":{title:"Državni zavod za statistiku Republike Hrvatske",publisher:"Državni zavod za statistiku",url:"https://dzs.hr/",type:"official",use:["stanovništvo","statistika","demografija","gospodarstvo"]},
+  "enciklopedija":{title:"Hrvatska enciklopedija",publisher:"Leksikografski zavod Miroslav Krleža",url:"https://www.enciklopedija.hr/",type:"reference",use:["povijest","osobe","mjesta","kultura"]},
+  "hazu":{title:"Hrvatska akademija znanosti i umjetnosti",publisher:"HAZU",url:"https://www.hazu.hr/",type:"reference",use:["znanost","kultura","povijesni izvori"]},
+  "min-kultura":{title:"Ministarstvo kulture i medija Republike Hrvatske",publisher:"Ministarstvo kulture i medija RH",url:"https://min-kulture.gov.hr/",type:"official",use:["kulturna baština","zaštićena dobra","nematerijalna baština"]},
+  "unesco":{title:"UNESCO World Heritage Centre",publisher:"UNESCO",url:"https://whc.unesco.org/",type:"international-reference",use:["svjetska baština"]},
+  "vatican":{title:"The Holy See",publisher:"Sveta Stolica",url:"https://www.vatican.va/",type:"official",use:["katolička baština","papinski dokumenti"]},
+  "hbk":{title:"Hrvatska biskupska konferencija",publisher:"HBK",url:"https://hbk.hr/",type:"official",use:["crkveni život","katolička baština"]}
+};
