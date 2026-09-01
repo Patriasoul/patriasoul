21fa10c04b62c452340d0b51f58e98a95dc109a1
Patriasoul <patriasoul@protonmail.com>
2026-09-01 21:36:10 +0200

    Popuni postrojbe.js centralnim registrom vojno-redarstvenih postrojbi

--- postrojbe.js
+++ postrojbe.js
@@ -1,1 +1,219 @@
-export const postrojbe = [];
+// PatriaSoul — središnji registar hrvatskih vojno-redarstvenih postrojbi
+//
+// Jedinstveni izvor podataka za Branitelje, Domovinu, Povijest, kvizove,
+// stranice postrojbi i buduće profile. Podaci su strukturirani tako da se
+// isti registar može prikazivati u različitim dijelovima projekta bez
+// dupliciranja sadržaja.
+
+export const postrojbe = [
+  {
+    id: "1-gardijska-brigada-tigrovi",
+    naziv: "1. gardijska brigada Tigrovi",
+    nadimak: "Tigrovi",
+    vrsta: "gardijska brigada",
+    sastav: "Hrvatska vojska / Zbor narodne garde",
+    godinaUstrojavanja: 1990,
+    pocetakDjelovanja: "1990-11-05",
+    ratniPut: ["Pakrac", "Plitvice", "Vukovar", "Banovina", "zapadna Slavonija", "Dubrovnik", "Lika", "Maslenica", "Bljesak", "Skok-2", "Ljeto 95", "Oluja"],
+    slogan: "Za Hrvatsku kao Tigar",
+    znacaj: "Jedna od temeljnih gardijskih brigada Hrvatske vojske i jedna od najpoznatijih postrojbi Domovinskog rata.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["gardijska", "HV", "Domovinski rat"]
+  },
+  {
+    id: "2-gardijska-brigada-gromovi",
+    naziv: "2. gardijska brigada Gromovi",
+    nadimak: "Gromovi",
+    vrsta: "gardijska brigada",
+    sastav: "Hrvatska vojska / Zbor narodne garde",
+    godinaUstrojavanja: 1991,
+    ratniPut: ["Banovina", "Pokuplje", "Posavina", "Dubrovnik", "Maslenica", "Bljesak", "Ljeto 95", "Oluja"],
+    znacaj: "Gardijska brigada Hrvatske vojske koja je sudjelovala u obrani i oslobađanju brojnih bojišta tijekom Domovinskog rata.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["gardijska", "HV", "Domovinski rat"]
+  },
+  {
+    id: "3-gardijska-brigada-kune",
+    naziv: "3. gardijska brigada Kune",
+    nadimak: "Kune",
+    vrsta: "gardijska brigada",
+    sastav: "Hrvatska vojska / Zbor narodne garde",
+    godinaUstrojavanja: 1991,
+    ratniPut: ["istočna Slavonija", "Vinkovci", "Vukovar", "Posavina", "Maslenica", "Bljesak", "Ljeto 95", "Oluja"],
+    znacaj: "Gardijska brigada posebno vezana uz obranu istočne Slavonije i sudjelovanje u ključnim operacijama Hrvatske vojske.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["gardijska", "HV", "Domovinski rat"]
+  },
+  {
+    id: "4-gardijska-brigada-pauci",
+    naziv: "4. gardijska brigada Pauci",
+    nadimak: "Pauci",
+    vrsta: "gardijska brigada",
+    sastav: "Hrvatska vojska / Zbor narodne garde",
+    godinaUstrojavanja: 1991,
+    ratniPut: ["južno bojište", "Dubrovnik", "Zadar", "Maslenica", "Medački džep", "Bljesak", "Ljeto 95", "Oluja"],
+    znacaj: "Gardijska brigada Hrvatske vojske iz Dalmacije koja je sudjelovala u obrani juga Hrvatske i u nizu oslobodilačkih operacija.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["gardijska", "HV", "Dalmacija", "Domovinski rat"]
+  },
+  {
+    id: "5-gardijska-brigada-sokolovi",
+    naziv: "5. gardijska brigada Sokolovi",
+    nadimak: "Sokolovi",
+    vrsta: "gardijska brigada",
+    sastav: "Hrvatska vojska",
+    godinaUstrojavanja: 1992,
+    ratniPut: ["istočna Slavonija", "Bosanska Posavina", "Maslenica", "Bljesak", "Ljeto 95", "Oluja"],
+    znacaj: "Gardijska brigada Hrvatske vojske čiji je ratni put bio posebno povezan s istočnom Slavonijom i završnim oslobodilačkim operacijama.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["gardijska", "HV", "Slavonija", "Domovinski rat"]
+  },
+  {
+    id: "7-gardijska-brigada-pume",
+    naziv: "7. gardijska brigada Pume",
+    nadimak: "Pume",
+    vrsta: "gardijska brigada",
+    sastav: "Hrvatska vojska",
+    godinaUstrojavanja: 1992,
+    ratniPut: ["južno bojište", "Maslenica", "Medački džep", "Bljesak", "Skok-2", "Ljeto 95", "Oluja"],
+    znacaj: "Gardijska brigada Hrvatske vojske poznata po sudjelovanju u važnim operacijama od južnog bojišta do završnih operacija 1995.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["gardijska", "HV", "Domovinski rat"]
+  },
+  {
+    id: "9-gardijska-brigada-vukovi",
+    naziv: "9. gardijska brigada Vukovi",
+    nadimak: "Vukovi",
+    vrsta: "gardijska brigada",
+    sastav: "Hrvatska vojska",
+    godinaUstrojavanja: 1992,
+    ratniPut: ["Lika", "Gospić", "Medački džep", "Bljesak", "Ljeto 95", "Oluja"],
+    znacaj: "Gardijska brigada iz Like koja je imala važnu ulogu u obrani ličkog bojišta i završnim oslobodilačkim operacijama.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["gardijska", "HV", "Lika", "Domovinski rat"]
+  },
+  {
+    id: "8-lakojurišna-brigada-vojne-policije-orlovi",
+    naziv: "8. lakojurišna brigada Vojne policije",
+    nadimak: "Orlovi",
+    vrsta: "lakojurišna brigada Vojne policije",
+    sastav: "Hrvatska vojska / Vojna policija",
+    godinaUstrojavanja: 1992,
+    ratniPut: ["razna bojišta Republike Hrvatske", "Maslenica", "Bljesak", "Ljeto 95", "Oluja"],
+    znacaj: "Specijalizirana vojno-policijska postrojba koja je izvršavala vojnopolicijske i borbene zadaće tijekom Domovinskog rata.",
+    kategorija: "Vojna policija",
+    oznake: ["vojna policija", "HV", "Domovinski rat"]
+  },
+  {
+    id: "1-hrvatski-gardijski-zdrug",
+    naziv: "1. hrvatski gardijski zdrug",
+    nadimak: "Zdrug",
+    vrsta: "gardijska postrojba posebne namjene",
+    sastav: "Hrvatska vojska / MORH / Glavni stožer OSRH",
+    godinaUstrojavanja: 1994,
+    pocetakDjelovanja: "1994-04-18",
+    ratniPut: ["Dubrovnik", "Vukovar", "razna bojišta", "Bljesak", "Ljeto 95", "Oluja"],
+    znacaj: "Elitna postrojba višenamjenskog karaktera namijenjena najzahtjevnijim zadaćama na različitim bojištima.",
+    kategorija: "Specijalne postrojbe",
+    oznake: ["elitna", "specijalna", "HV", "Domovinski rat"]
+  },
+  {
+    id: "bojne-zrinski",
+    naziv: "Bojna Zrinski",
+    nadimak: "Zrinski",
+    vrsta: "specijalna postrojba",
+    sastav: "MORH / Hrvatska vojska",
+    godinaUstrojavanja: 1991,
+    ratniPut: ["razna bojišta", "specijalne zadaće"],
+    znacaj: "Jedna od temeljnih specijalnih postrojbi MORH-a koja je sudjelovala u stvaranju i razvoju hrvatskih specijalnih snaga.",
+    kategorija: "Specijalne postrojbe",
+    oznake: ["specijalna", "MORH", "Domovinski rat"]
+  },
+  {
+    id: "bojna-matija-vlacic",
+    naziv: "Bojna Matija Vlačić",
+    nadimak: "Matija Vlačić",
+    vrsta: "specijalna postrojba",
+    sastav: "MORH / Hrvatska vojska",
+    godinaUstrojavanja: 1992,
+    ratniPut: ["razna bojišta", "specijalne zadaće"],
+    znacaj: "Specijalna postrojba MORH-a koja je zajedno s Bojnom Zrinski činila jednu od sastavnica 1. hrvatskog gardijskog zdruga.",
+    kategorija: "Specijalne postrojbe",
+    oznake: ["specijalna", "MORH", "Domovinski rat"]
+  },
+  {
+    id: "specijalna-jedinica-lucko",
+    naziv: "Antiteroristička jedinica Lučko",
+    nadimak: "ATJ Lučko",
+    vrsta: "antiteroristička policijska jedinica",
+    sastav: "Ministarstvo unutarnjih poslova Republike Hrvatske",
+    godinaUstrojavanja: 1990,
+    ratniPut: ["Pakrac", "Plitvice", "Banovina", "Kordun", "Lika", "Dubrovnik", "razna bojišta"],
+    znacaj: "Jedna od ključnih specijalnih policijskih postrojbi Republike Hrvatske koja je sudjelovala u obrambenim i oslobodilačkim djelovanjima tijekom Domovinskog rata.",
+    kategorija: "Specijalna policija MUP-a",
+    oznake: ["MUP", "specijalna policija", "Domovinski rat"]
+  },
+  {
+    id: "alfa",
+    naziv: "Jedinica za posebne zadatke MUP-a Alfa",
+    nadimak: "Alfa",
+    vrsta: "specijalna policijska jedinica",
+    sastav: "Ministarstvo unutarnjih poslova Republike Hrvatske",
+    godinaUstrojavanja: 1990,
+    ratniPut: ["razna bojišta", "Banovina", "Lika", "Dubrovnik", "oslobodilačke operacije"],
+    znacaj: "Specijalna policijska postrojba MUP-a RH koja je sudjelovala u obrani Republike Hrvatske i važnim operacijama tijekom Domovinskog rata.",
+    kategorija: "Specijalna policija MUP-a",
+    oznake: ["MUP", "specijalna policija", "Domovinski rat"]
+  },
+  {
+    id: "grom",
+    naziv: "Jedinica za posebne zadatke MUP-a Grom",
+    nadimak: "Grom",
+    vrsta: "specijalna policijska jedinica",
+    sastav: "Ministarstvo unutarnjih poslova Republike Hrvatske",
+    godinaUstrojavanja: 1990,
+    ratniPut: ["Banovina", "Petrinja", "Sisak", "Kordun", "razna bojišta"],
+    znacaj: "Specijalna policijska postrojba koja je sudjelovala u obrani i oslobađanju Republike Hrvatske tijekom Domovinskog rata.",
+    kategorija: "Specijalna policija MUP-a",
+    oznake: ["MUP", "specijalna policija", "Domovinski rat"]
+  },
+  {
+    id: "zbor-narodne-garde",
+    naziv: "Zbor narodne garde Republike Hrvatske",
+    nadimak: "ZNG",
+    vrsta: "oružana postrojba",
+    sastav: "Republika Hrvatska",
+    godinaUstrojavanja: 1991,
+    pocetakDjelovanja: "1991-05-28",
+    ratniPut: ["cijela Republika Hrvatska"],
+    znacaj: "Prva profesionalna oružana postrojba Republike Hrvatske s obrambenim i redarstvenim zadaćama, iz koje se razvijala Hrvatska vojska.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["ZNG", "HV", "temeljna postrojba", "Domovinski rat"]
+  },
+  {
+    id: "hrvatska-ratna-mornarica",
+    naziv: "Hrvatska ratna mornarica",
+    nadimak: "HRM",
+    vrsta: "oružana sastavnica",
+    sastav: "Hrvatska vojska",
+    godinaUstrojavanja: 1991,
+    ratniPut: ["Jadransko more", "južno bojište", "Dubrovnik", "obalno područje"],
+    znacaj: "Pomorska sastavnica Hrvatske vojske koja je tijekom Domovinskog rata sudjelovala u obrani hrvatskog Jadrana i pomorskim zadaćama.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["HRM", "mornarica", "Domovinski rat"]
+  },
+  {
+    id: "hrvatsko-ratno-zrakoplovstvo",
+    naziv: "Hrvatsko ratno zrakoplovstvo",
+    nadimak: "HRZ",
+    vrsta: "oružana sastavnica",
+    sastav: "Hrvatska vojska",
+    godinaUstrojavanja: 1991,
+    ratniPut: ["razna bojišta Republike Hrvatske", "Bljesak", "Oluja"],
+    znacaj: "Zračna sastavnica Hrvatske vojske koja je tijekom Domovinskog rata pružala potporu kopnenim snagama i izvršavala borbene i transportne zadaće.",
+    kategorija: "Hrvatska vojska",
+    oznake: ["HRZ", "zrakoplovstvo", "Domovinski rat"]
+  }
+];
+
+export default postrojbe;
