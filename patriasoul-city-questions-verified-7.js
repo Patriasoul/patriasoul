731451da3caa7b3996b27f00324cdf9976f73406
Patriasoul <patriasoul@protonmail.com>
2026-09-02 12:55:02 +0200

    Zamijeni nastavak 7 provjerenim pitanjima za Dugu Resu, Ogulin, Ozalj i Slunj

--- patriasoul-city-questions-verified-7.js
+++ patriasoul-city-questions-verified-7.js
@@ -2,37 +2,32 @@
 // Nije nova glavna banka; pitanja pripadaju sloju Brani svoj grad.
 (function(global){'use strict';
   const extra={
-    glina:[
-      ['Koje se godine Glina prvi put spominje u povijesnim izvorima?',['1284.','1209.','1242.','1334.']],
-      ['Po čemu je Glina dobila ime?',['Po istoimenoj rijeci','Po plemićkoj obitelji','Po utvrdi','Po starom sajmištu']],
-      ['Koji je kasniji hrvatski ban službovao u Glini od 1841. do početka 1848.?',['Josip Jelačić','Ivan Mažuranić','Josip Šokčević','Nikola Zrinski']],
-      ['Koji je grad u Domovinskom ratu bio povezan s predajom 21. korpusa 1995. godine?',['Glina','Petrinja','Sisak','Novska']]
+    'duga-resa':[
+      ['Koje se godine Duga Resa prvi put spominje kao malo mjesto?',['1380.','1209.','1477.','1500.']],
+      ['Koje je godine osnovana Pamučna industrija Duga Resa?',['1884.','1873.','1896.','1901.']],
+      ['Koja rijeka predstavlja najpoznatiji prirodni simbol Duge Rese?',['Mrežnica','Kupa','Korana','Dobra']],
+      ['Koje je godine Duga Resa dobila službeni status grada?',['1993.','1896.','1873.','1990.']]
     ],
-    hrvatska_kostajnica:[
-      ['Na kojoj rijeci leži Hrvatska Kostajnica?',['Uni','Kupi','Savi','Glini']],
-      ['Koja je plemićka obitelj od 1528. do 1566. bila vlasnik Kostajnice?',['Zrinski','Frankapani','Erdődy','Keglevići']],
-      ['Kako je Hrvatska Kostajnica povijesno prozvana zbog otpora Osmanlijama?',['Branik domovine','Vrata Hrvatske','Čuvar Une','Tvrđava slobode']],
-      ['Po čemu je područje Hrvatske Kostajnice dobilo ime prema lokalnoj tradiciji?',['Po kestenima','Po kamenju','Po rijeci','Po staroj crkvi']]
+    'ogulin':[
+      ['S kojom je plemićkom obitelji povezano utemeljenje Ogulina?',['Frankopanima','Zrinskima','Erdődyjima','Šubićima']],
+      ['Oko koje je godine Bernardin Frankopan dao izgraditi ogulinski kaštel?',['Oko 1500.','Oko 1400.','Oko 1600.','Oko 1700.']],
+      ['Uz koju se rijeku nalazi Stari grad Ogulin?',['Dobru','Mrežnicu','Kupu','Koranu']],
+      ['Koja je poznata hrvatska književnica rođena u Ogulinu?',['Ivana Brlić-Mažuranić','Marija Jurić Zagorka','Dragojla Jarnević','Jagoda Truhelka']]
     ],
-    novska:[
-      ['Koje se godine u popisu župa spominje najstariji naziv povezan s Novskom, Bjelavina?',['1334.','1209.','1532.','1691.']],
-      ['Koja je plemićka obitelj držala novljansko područje prije osmanskih osvajanja?',['Svetački','Zrinski','Frankopan','Erdődy']],
-      ['Kako se zvala utvrda koju su Svetački podigli 1532. godine na području Novske?',['Novi odnosno Novigrad (Ujvár)','Kostajnica','Kraljeva Velika','Bijela Stijena']],
-      ['Do koje je godine novljansko područje ostalo pod osmanskom vlašću prema povijesti grada?',['1691.','1595.','1683.','1718.']]
+    'ozalj':[
+      ['Koje se godine ime Ozlja prvi put zapisuje u povijesnim izvorima?',['1244.','1209.','1258.','1398.']],
+      ['Koja je velikaška obitelj preuzela Ozalj 1550. godine?',['Zrinski','Frankopani','Babonići','Erdődy']],
+      ['Koji je ban Zrinski stolovao u Ozlju u 17. stoljeću?',['Petar Zrinski','Nikola Šubić Zrinski','Juraj IV. Zrinski','Nikola VII. Zrinski']],
+      ['Koje su godine pogubljeni Petar Zrinski i Fran Krsto Frankopan?',['1671.','1667.','1683.','1699.']]
     ],
-    petrinja:[
-      ['Koje je godine započela gradnja Nove Petrinje na današnjem mjestu?',['1592.','1531.','1594.','1753.']],
-      ['Tko je gradio novu tvrđavu Petrinju 1592. godine?',['Hasan-paša Predojević','Toma Erdődy','Nikola Zrinski','Ivan Drašković']],
-      ['Koje godine Petrinja postaje vojna općina?',['1765.','1753.','1689.','1881.']],
-      ['Koja je vojno-redarstvena operacija 1995. bila ključna za oslobođenje Petrinje?',['Oluja','Bljesak','Maslenica','Medački džep']]
+    'slunj':[
+      ['Koje se godine Slunj prvi put spominje u pisanim izvorima kao utvrđeni grad?',['1390.','1323.','1409.','1579.']],
+      ['Koja je plemićka obitelj izgradila Slunj kao jedno od svojih središta?',['Frankopani','Zrinski','Babonići','Erdődy']],
+      ['Kako se zove poznato naselje na sutoku Slunjčice i Korane?',['Rastoke','Točak','Primišlje','Tržac']],
+      ['Koje su godine hrvatske snage oslobodile Slunj u operaciji Oluja?',['1995.','1991.','1992.','1994.']]
     ]
   };
-  const urls={
-    glina:'https://www.grad-glina.hr/povijest-gline/',
-    hrvatska-kostajnica:'https://hrvatska-kostajnica.hr/wp-content/uploads/2019/08/Strateski-razvojni-program-Grada-Hrvatske-Kostajnice-2018.-2023.-1.pdf',
-    novska:'https://old.novska.hr/hr/o-novskoj/povijest/',
-    petrinja:'https://petrinja.hr/povijest-grada/'
-  };
+  const urls={'duga-resa':'https://dugaresa.hr/povijest-grada/','ogulin':'https://www.ogulin.hr/o-ogulinu/povijest/','ozalj':'https://ozalj.hr/o-gradu/','slunj':'https://slunj.hr/povijest/'};
   const normalizeCity=s=>String(s).toLocaleLowerCase('hr').normalize('NFD').replace(/[\u0300-\u036f]/g,'').replace(/đ/g,'d').replace(/[^a-z0-9]+/g,'-').replace(/^-|-$/g,'');
   const all=[];
   Object.entries(extra).forEach(([city,items])=>items.forEach((x,i)=>all.push({id:`verified7_${city}_${String(i+1).padStart(3,'0')}`,cityId:normalizeCity(city),citySource:'verified',category:'gradovi',question:x[0],answers:x[1],correctIndex:0,sourceUrl:urls[normalizeCity(city)]})));
