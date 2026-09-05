36eae7911e20965274319b9c8eb58be1e5b27d08
Patriasoul <patriasoul@protonmail.com>
2026-09-05 20:07:29 +0200

    Dodaj uredničke profile za sljedećih 10 gradova

---
+++ gradovi-profil-2.js
@@ -0,0 +1,103 @@
+/* PatriaSoul — urednički registar profila gradova, serija 2 */
+window.PATRIA_CITY_EDITORIAL_2 = {
+  "Dubrovnik": {
+    intro: "Dubrovnik je povijesni grad na južnom Jadranu i jedno od najvažnijih hrvatskih središta kulturne i pomorske baštine.",
+    geography: "Smješten je na krajnjem jugu hrvatske obale, uz strme padine Srđa i more, s povijesnom jezgrom zaštićenom snažnim fortifikacijskim sustavom.",
+    history: "Dubrovnik se razvio iz ranosrednjovjekovnog naselja i kroz stoljeća izgradio vlastitu političku i trgovačku tradiciju Dubrovačke Republike. Grad je bio važno pomorsko i diplomatsko središte Mediterana, a povijesna jezgra danas je pod zaštitom UNESCO-a.",
+    heritage: ["Dubrovačke gradske zidine", "Stradun", "Knežev dvor", "Katedrala Uznesenja Marijina", "Tvrđava Lovrijenac"],
+    people: ["Marin Držić", "Ruđer Bošković", "Ivan Gundulić", "Marin Getaldić"],
+    defence: "Dubrovnik je tijekom Domovinskog rata bio izložen napadima, osobito tijekom 1991. godine. Obrana grada i šireg dubrovačkog područja postala je jedan od važnih dijelova obrane juga Hrvatske.",
+    faith: "Dubrovačka katedrala, crkva sv. Vlaha i franjevački samostan čine važan dio višestoljetnog katoličkog identiteta grada.",
+    sources: ["UNESCO — Old City of Dubrovnik", "Grad Dubrovnik — službeni izvori"]
+  },
+  "Vukovar": {
+    intro: "Vukovar je grad na Dunavu i jedno od najvažnijih mjesta hrvatskog nacionalnog sjećanja na Domovinski rat.",
+    geography: "Grad se nalazi na ušću Vuke u Dunav, u istočnoj Hrvatskoj, na prostoru Vukovarsko-srijemske županije.",
+    history: "Vukovar ima dugu povijest naseljenosti i važan položaj uz Dunav. Posebno mjesto u suvremenoj povijesti zauzima 1991., kada je grad tijekom Domovinskog rata bio pod opsadom i pretrpio velika razaranja. Nakon mirne reintegracije hrvatskog Podunavlja Vukovar je obnovljen, ali je sjećanje na rat ostalo sastavni dio njegova identiteta.",
+    heritage: ["Vukovarski vodotoranj", "Dvorac Eltz", "Muzej vučedolske kulture", "Memorijalno groblje žrtava iz Domovinskog rata", "Vučedol"],
+    people: ["Lavoslav Ružička", "Siniša Glavašević", "lokalne žrtve, branitelji i svjedoci 1991."],
+    defence: "Vukovar je 1991. postao simbol otpora i stradanja Hrvatske. Obrana grada trajala je mjesecima, a nakon pada uslijedili su zločini i masovna stradanja. Memorijalni sadržaj treba povezivati s provjerenim izvorima i institucijama sjećanja.",
+    faith: "Crkva sv. Filipa i Jakova te obnovljena sakralna baština svjedoče o dugom katoličkom kontinuitetu Vukovara.",
+    sources: ["Grad Vukovar — službeni izvori", "Memorijalni centar Domovinskog rata Vukovar", "UNESCO — Vučedol / kulturna baština"]
+  },
+  "Bjelovar": {
+    intro: "Bjelovar je regionalno središte Bjelovarsko-bilogorske županije i grad planski razvijen u sklopu vojnog sustava 18. stoljeća.",
+    geography: "Smješten je na ravničarskom i blago brežuljkastom prostoru Bilogore, u sjeverozapadnoj Hrvatskoj.",
+    history: "Bjelovar je osnovan 1756. odlukom carice Marije Terezije kao novo vojno i upravno središte Varaždinskog generalata. Planska struktura središta grada i danas podsjeća na njegovu vojnu prošlost.",
+    heritage: ["Trg Eugena Kvaternika", "Katedrala sv. Terezije Avilske", "Gradski muzej Bjelovar", "Paviljon u središnjem parku", "povijesna planska jezgra"],
+    people: ["Eugen Kvaternik", "Antun Nemčić", "lokalni kulturni i vojni djelatnici"],
+    defence: "Bjelovar je u Domovinskom ratu imao važnu ulogu u obrani sjeverozapadne Hrvatske. Grad je povezan s događajima 1991. i oslobađanjem vojnih objekata te ustrojem hrvatskih postrojbi.",
+    faith: "Katedrala sv. Terezije Avilske jedno je od središnjih mjesta katoličkog života Bjelovara.",
+    sources: ["Grad Bjelovar — službeni izvori"]
+  },
+  "Čakovec": {
+    intro: "Čakovec je središte Međimurja, grad snažne fortifikacijske, kulturne i gospodarske tradicije na sjeveru Hrvatske.",
+    geography: "Smješten je između rijeka Mure i Drave, u sjevernom dijelu Hrvatske uz granicu sa Slovenijom i Mađarskom.",
+    history: "Razvoj Čakovca snažno je povezan s utvrdom Zrinskih i obitelji Zrinski. Stari grad Zrinskih postao je jezgra urbanog razvoja, a Međimurje je kroz stoljeća bilo važno pogranično područje.",
+    heritage: ["Stari grad Zrinskih", "Muzej Međimurja", "Perivoj Zrinskih", "Crkva sv. Nikole", "središte Čakovca"],
+    people: ["Nikola Zrinski Čakovečki", "Petar Zrinski", "Franjo Ksaver Pahner"],
+    defence: "Međimurje i Čakovec dali su doprinos obrani Hrvatske kroz sudjelovanje građana i pripadnika hrvatskih postrojbi tijekom Domovinskog rata.",
+    faith: "Crkva sv. Nikole i franjevačka tradicija dio su vjerske baštine Čakovca.",
+    sources: ["Grad Čakovec — službeni izvori", "Muzej Međimurja Čakovec"]
+  },
+  "Koprivnica": {
+    intro: "Koprivnica je povijesno središte Podravine, grad čiji su razvoj obilježili srednjovjekovni status slobodnog kraljevskog grada, tvrđava i trgovina.",
+    geography: "Grad se nalazi u Podravini, u blizini Drave, na važnim prometnim pravcima između sjeverne Hrvatske i srednje Europe.",
+    history: "Koprivnica se u pisanim izvorima spominje u 13. stoljeću, a 1356. poveljom kralja Ludovika I. potvrđen joj je status slobodnog i kraljevskog grada. U novom vijeku važna je bila renesansna tvrđava, dok je željeznica u 19. stoljeću potaknula novi gospodarski razvoj.",
+    heritage: ["Koprivnička tvrđava", "Gradski trg", "Muzej grada Koprivnice", "srednjovjekovna i barokna urbana baština", "podravska tradicijska kultura"],
+    people: ["Ivan Generalić", "Mihovil Pavlek Miškina", "Podravski naivni umjetnici"],
+    defence: "Koprivnica i Podravina sudjelovali su u obrani Hrvatske kroz ustroj i logistiku hrvatskih postrojbi te angažman lokalnog stanovništva.",
+    faith: "Župna crkva sv. Nikole i franjevačka tradicija važan su dio duhovne baštine grada.",
+    sources: ["Grad Koprivnica — Povijest grada"]
+  },
+  "Križevci": {
+    intro: "Križevci su jedan od najstarijih hrvatskih urbanih centara, s važnom srednjovjekovnom političkom, crkvenom i kulturnom ulogom.",
+    geography: "Grad se nalazi u sjeverozapadnoj Hrvatskoj, na prijelazu između Kalničkog prigorja i podravskog prostora.",
+    history: "Križevci su tijekom srednjeg vijeka imali važnu ulogu u hrvatskom političkom životu i razvili se kao slobodni kraljevski grad. Povijest grada povezana je s održavanjem sabora i s razvojem crkvenih i obrazovnih ustanova.",
+    heritage: ["Gradske crkve i povijesna jezgra", "Grkokatolička katedrala Presvetog Trojstva", "Križevački gradski muzej", "Kalnik i okolni krajolik"],
+    people: ["Marcel Kiepach", "Milutin Mayer", "Ivan Zakmardi de Diankovec"],
+    defence: "Stanovnici Križevaca i okolice sudjelovali su u obrani Hrvatske tijekom Domovinskog rata, dok je grad imao regionalnu logističku ulogu.",
+    faith: "Križevci su posebno važni po grkokatoličkoj tradiciji i sjedištu Križevačke eparhije.",
+    sources: ["Grad Križevci — službeni izvori", "Križevačka eparhija"]
+  },
+  "Sisak": {
+    intro: "Sisak je povijesni grad na Kupi, Odri i Savi, poznat po antičkoj Sisciji, industrijskoj baštini i važnoj obrambenoj ulozi kroz hrvatsku povijest.",
+    geography: "Smješten je na ušću Kupe u Savu, na važnom riječnom i kopnenom prostoru središnje Hrvatske.",
+    history: "Rimska Siscia bila je jedno od važnih središta Panonije. U kasnijim razdobljima Sisak je bio važna utvrda i trgovačko središte. Njegova tvrđava svjedoči o obrani od Osmanlija, a bitka kod Siska 1593. zauzima posebno mjesto u hrvatskoj povijesti.",
+    heritage: ["Stari grad Sisak", "Arheološki park Siscia", "Katedrala Uzvišenja Svetog Križa", "Holandska kuća", "industrijska baština uz Kupu"],
+    people: ["Antun Vrančić", "Ivan Kukuljević Sakcinski", "lokalni industrijalci i kulturni djelatnici"],
+    defence: "Sisak je tijekom Domovinskog rata bio važno bojište Banovine i sisačkog područja. Grad i okolna mjesta pretrpjeli su napade, a hrvatske snage branile su ključne prometne i industrijske pravce.",
+    faith: "Katedrala Uzvišenja Svetog Križa i bogata povijest Sisačke biskupije čine važan dio identiteta grada.",
+    sources: ["Grad Sisak — službeni izvori", "Muzej Sisak"]
+  },
+  "Požega": {
+    intro: "Požega je povijesno središte Požeške kotline, grad barokne jezgre i važna kulturna točka Slavonije.",
+    geography: "Grad se nalazi u Požeškoj kotlini okruženoj slavonskim gorama, u središtu Požeško-slavonske županije.",
+    history: "Požega ima srednjovjekovne korijene, a kroz osmansko i habsburško razdoblje mijenjala je svoj politički i urbani položaj. U novom vijeku razvija se kao regionalno upravno, crkveno i kulturno središte.",
+    heritage: ["Trg Svetog Trojstva", "Katedrala sv. Terezije Avilske", "Franjevački samostan", "Gradski muzej Požega", "barokna povijesna jezgra"],
+    people: ["Matko Peić", "Miroslav Kraljević", "lokalni kulturni djelatnici"],
+    defence: "Požega je tijekom Domovinskog rata bila regionalno središte za organizaciju obrane i logističku potporu hrvatskim snagama.",
+    faith: "Katedrala sv. Terezije Avilske i franjevačka tradicija imaju važnu ulogu u vjerskom i kulturnom životu grada.",
+    sources: ["Grad Požega — službeni izvori"]
+  },
+  "Virovitica": {
+    intro: "Virovitica je regionalno središte Podravine i grad čiji su razvoj obilježili srednjovjekovni korijeni, plemićka baština i položaj prema Dravi.",
+    geography: "Grad leži na dodiru ravničarske Podravine i obronaka Bilogore, u blizini hrvatsko-mađarskog prostora.",
+    history: "Virovitica se razvijala kao srednjovjekovno naselje i važno upravno mjesto. U novom vijeku prostor je bio povezan s obrambenim sustavom Slavonije, a kasnije s gospodarskim i prometnim razvojem Podravine.",
+    heritage: ["Dvorac Pejačević", "Gradski park", "Crkva sv. Roka", "Gradski muzej Virovitica", "podravski krajolik"],
+    people: ["Petar Preradović", "grofovi Pejačević", "lokalni kulturni djelatnici"],
+    defence: "Virovitica je u Domovinskom ratu imala važnu ulogu u obrani zapadne Slavonije i ustroju hrvatskih postrojbi.",
+    faith: "Crkva sv. Roka i župna tradicija predstavljaju važan dio duhovnog života Virovitice.",
+    sources: ["Grad Virovitica — službeni izvori", "Gradski muzej Virovitica"]
+  },
+  "Krapina": {
+    intro: "Krapina je povijesno središte Hrvatskog zagorja, poznata po neandertalskom nalazištu, staroj gradskoj jezgri i kulturnoj baštini Zagorja.",
+    geography: "Grad se nalazi u dolini Krapinčice, među brežuljcima Hrvatskog zagorja, blizu granice sa Slovenijom.",
+    history: "Krapinsko područje poznato je po arheološkom nalazištu Hušnjakovo, gdje su pronađeni važni ostaci neandertalaca. Srednjovjekovna Krapina razvila se kao utvrđeno naselje i regionalno središte Zagorja.",
+    heritage: ["Muzej krapinskih neandertalaca", "Hušnjakovo", "Stari grad Krapina", "Crkva sv. Nikole", "zagorska tradicijska baština"],
+    people: ["Dragutin Gorjanović-Kramberger", "Ljudevit Vukotinović", "lokalni zagorski kulturni djelatnici"],
+    defence: "Stanovnici Krapine i Hrvatskog zagorja sudjelovali su u obrani Hrvatske tijekom Domovinskog rata, a područje je bilo važno zaleđe i logistički prostor.",
+    faith: "Župna crkva sv. Nikole i brojna svetišta Hrvatskog zagorja dio su duhovne baštine krapinskog kraja.",
+    sources: ["Grad Krapina — službeni izvori", "Muzeji Hrvatskog zagorja — Muzej krapinskih neandertalaca"]
+  }
+};
