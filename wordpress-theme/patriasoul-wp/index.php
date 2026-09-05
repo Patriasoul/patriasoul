116536bcb41b25b15bede76b73741b79817eeabc
Patriasoul <patriasoul@protonmail.com>
2026-09-05 11:09:19 +0200

    Add WordPress fallback template

---
+++ wordpress-theme/patriasoul-wp/index.php
@@ -0,0 +1,1 @@
+<?php get_header(); ?><main class="ps-section"><div class="ps-wrap"><?php if(have_posts()):while(have_posts()):the_post(); ?><article><h1><?php the_title(); ?></h1><?php the_content(); ?></article><?php endwhile;else:?><p>Nema sadržaja.</p><?php endif; ?></div></main><?php get_footer(); ?>
