d45c9450332764e0d7b3b4db5f202a6366324b18
Patriasoul <patriasoul@protonmail.com>
2026-09-11 14:36:23 +0200

    Dodaj službeni sloj dopune za ratne brigade

---
+++ brigade-dopuna.js
@@ -0,0 +1,36 @@
+// PatriaSoul – dodatne službeno dokumentirane ratne brigade
+// Ovaj sloj dopunjuje glavni registar samo kada je postrojba potvrđena
+// u službenoj građi MORH-a. Ne služi za nagađanje ili popunjavanje rupa.
+export const BRIGADE_DOPUNA = [
+  {
+    id: 155,
+    number: '155.',
+    name: '155. brigada HV',
+    nickname: '',
+    type: 'pričuvna',
+    headquarters: 'Crikvenica',
+    region: 'Kvarner i Primorje',
+    period: '1991–1995',
+    source: 'MORH – Znakovlje pobjednika',
+    url: 'https://znakovlje.morh.hr/category/ratne-brigade/'
+  }
+];
+
+export const BRIGADE_SLUZBENI_IZVORI = [
+  {
+    title: 'MORH – Znakovlje pobjednika: Ratne brigade',
+    url: 'https://znakovlje.morh.hr/ratne-brigade/'
+  },
+  {
+    title: 'MORH – Postroj zastava ratnih postrojbi',
+    url: 'https://www.morh.hr/postroj-zastava-ratnih-postrojbi/'
+  },
+  {
+    title: 'HMDCDR – Izdavačka djelatnost',
+    url: 'https://centardomovinskograta.hr/djelatnost/izdavacka-djelatnost/'
+  },
+  {
+    title: 'HMDCDR – Fotomonografije',
+    url: 'https://centardomovinskograta.hr/djelatnost/izdavacka-djelatnost/fotomonografije/'
+  }
+];
